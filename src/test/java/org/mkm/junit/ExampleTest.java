8bb34d8c95fc5f8056d2380233d3023f4fa80544
mourav2222 <mourav2222@gmail.com>
2023-02-19 12:48:40 +0100

    Initial commit

---
+++ src/test/java/org/mkm/junit/ExampleTest.java
@@ -0,0 +1,11 @@
+package org.mkm.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+    @Test
+    void shouldShowSimpleAssertion() {
+        Assertions.assertEquals(1, 1);
+    }
+}
